d2bc2569f8bcce50a9a7ae8107a1963f455ceba3
SatyajitChoudhary <satyajitchoudhary09@gmail.com>
2018-08-05 19:29:27 +0530

    Redux Form implemented part-2 reset function added after submitting feedback form in contact us and the state of the form is stored even on navigation to other tab

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -5,15 +5,20 @@ import logger from 'redux-logger';
 import { Comments } from './comments';
 import { Promotions } from './promotions';
 import { Leaders } from './leaders';
+import { createForms } from 'react-redux-form';
+import { InitialFeedback } from './forms';
 
 export const ConfigureStore = () => {
     const store = createStore(
       combineReducers({
-          dishes: Dishes,
-          comments: Comments,
-          promotions: Promotions,
-          leaders: Leaders
-      }),
+            dishes: Dishes,
+            comments: Comments,
+            promotions: Promotions,
+            leaders: Leaders,
+            ...createForms({
+                feedback: InitialFeedback
+            })
+        }),
       applyMiddleware(thunk, logger)
     );
 
